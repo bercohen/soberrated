3d1bd2832a9c552dac4b586691c47ee89b75f7c4
Ber <bercohen1@gmail.com>
2017-11-29 22:50:12 -0500

    dbug1

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,10 +9,11 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
-//= require jquery
-//= require jquery_ujs
-//= require bootstrap
+// causes carousel to run
+// = require jquery
+// = require jquery_ujs
+// = require bootstrap
+
 // = require bootstrap-sprockets
 //= require_tree .
 
